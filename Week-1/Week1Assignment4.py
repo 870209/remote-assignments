40432ed9c1b4f6624938398265f81ad00437f6e4
Aaron <97728939+870209@users.noreply.github.com>
2022-03-30 18:07:34 +0800

    Add files via upload

---
+++ Week-1/Week1Assignment4.py
@@ -0,0 +1,17 @@
+def binary_search_position(numbers,target):
+    left=0
+    right=len(numbers)-1
+    while (left<=right):
+        mid=(left+right)//2
+        if numbers[mid]>target:
+            right = mid-1
+        elif numbers[mid]<target:
+            left=mid+1
+        else:
+            return mid
+
+    return -1
+
+
+print(binary_search_position([1, 2, 5, 6, 7], 1))
+print(binary_search_position([1, 2, 5, 6, 7], 6)) 
